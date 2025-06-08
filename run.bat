ca73b164edfaca36073e090e80ae16dcb53206e5
yihuan <ivan17.lai@gmail.com>
2025-06-08 14:28:49 +0800

    update

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
 git clone https://github.com/ivan17lai/paperSheet.llm.git
 cd paperSheet.llm
+
 pip install google-generativeai pandas openpyxl pillow
 python main.py
-pause
+@echo off
+start "" "results.xlsx"
+
